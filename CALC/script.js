d1129f88993b0c93d78d1726f73c7cb7d17b7b07
Nitish <Nitishrai7280@gmail.com>
2024-12-25 00:43:27 +0530

    Add files via upload

---
+++ CALC/script.js
@@ -0,0 +1,22 @@
+let string = '';
+
+let buttons = document.querySelectorAll('button');
+
+
+Array.from(buttons).forEach((button)=>{
+     button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == '=') {
+          string = eval(string)
+          document.getElementById('display').value = string;  
+        }else if(e.target.innerHTML == 'C'){
+           string = '';
+           document.getElementById('display').value = string;  
+        }
+        else {
+        console.log(e.target)
+         string = string + e.target.innerHTML;
+         document.getElementById('display').value = string;
+        }
+         
+     })
+})
